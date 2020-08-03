f2474403583163d0f0d2e0a66d01767da449c9ba
Chen <chenj9@mski2265.mskcc.root.mskcc.org>
2020-08-03 16:59:19 -0400

    add analysis in RawData.R

--- RawData.R
+++ RawData.R
@@ -9,15 +9,13 @@ counts = rds@raw.data
 ID2name = readRDS("/data/riazlab/projects/TCRseq/Input/ID2name.rds")
 dimnames(counts)[[1]] = ID2name$GeneName
 rds = CreateSeuratObject(counts = counts)
-
 rds <- NormalizeData(rds)
-
 rds <- FindVariableFeatures(rds)
 #### Batch correction #####
 rds@meta.data$Experiment = substr(rownames(rds@meta.data),20,25)
 library(SeuratWrappers)
 rds <- RunFastMNN(object.list = SplitObject(rds, split.by = "Experiment"))
-saveRDS(rds, file="/data/riazlab/projects/TCRseq/output/fastMNNcorrect.rds")
+#saveRDS(rds, file="/data/riazlab/projects/TCRseq/output/fastMNNcorrect.rds")
 #### Batch correction #####
 
 Sdataobj = rds
@@ -41,7 +39,7 @@ top10 <- head(VariableFeatures(Sdataobj), 10)
 pdf("/data/riazlab/projects/TCRseq/output/SigFeatures.v01.pdf", width = 14)
 plot1 <- VariableFeaturePlot(Sdataobj)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-  plot2
+plot2
 dev.off()
 
 # Scaling the data
@@ -114,3 +112,61 @@ pdf("/data/riazlab/projects/TCRseq/output/Violin.v01.pdf")
     VlnPlot(Sdataobj, features = c("Cd79a"), pt.size = 0.1)
     VlnPlot(Sdataobj, features = c("Ncr1"), pt.size = 0.1) # Cd335/ NKp46 NK cell
 dev.off()
+
+levels(x = Sdataobj)
+Sdataobj2 <- RenameIdents(object = Sdataobj, 
+    '0' = 'Cd4+ T cells 1', '1' = 'Cd8a+ T cells 1',
+    '2' = 'Cd79a+ B cells 1', '3' = 'Cd4+ T cells 2', '4' = 'Cd4+ T cells 3', 
+    '5' = 'Cd14 macrophages 1', '6' = 'Cd14 macrophages 2', '7' = 'Cd79a+ B cells 4', 
+    '8' = 'Cd4+ T cells 4', '9' = 'Ncr1+ NK cells 1', '10' = 'Lyz2+ Dendritic', 
+    '11' = 'Ncr1+ NK cells 2', '12' = 'Cd8a+ NK cells', '13' = '? Il7r+ cells', 
+    '14' = 'Ambiguous1', '15' = 'Cd8a+ T cells 2', '16' = 'Cd79a+ B cells 3', 
+    '17' = 'Ambiguous2', '18' = '? Cd8a Cd4 Ly6c1 Dendritic', 
+    '19' = 'Ambiguous3', '20' = 'Ambiguous4', '21' = 'Ambiguous5', 
+    '22' = '? Cd79a+ Ly6c1+', '23' = 'Cd79a+ B cells 2')
+levels(x = Sdataobj2)
+pdf("/data/riazlab/projects/TCRseq/output/tsne.lable.v00.pdf", width = 13)
+DimPlot(Sdataobj2, reduction = "tsne", label = T)
+dev.off()
+
+#### Compare clusters Fig5 D #####
+Cd4Tcell1 <- subset(Sdataobj2, idents = "Cd4+ T cells 1")
+Cd4Tcell2 <- subset(Sdataobj2, idents = "Cd4+ T cells 2")
+Cd4Tcell3 <- subset(Sdataobj2, idents = "Cd4+ T cells 3")
+Cd4Tcell4 <- subset(Sdataobj2, idents = "Cd4+ T cells 4")
+
+avg.c1 <- log1p(AverageExpression(Cd4Tcell1, verbose = FALSE)$RNA)
+avg.c2 <- log1p(AverageExpression(Cd4Tcell2, verbose = FALSE)$RNA)
+avg.c3 <- log1p(AverageExpression(Cd4Tcell3, verbose = FALSE)$RNA)
+avg.c4 <- log1p(AverageExpression(Cd4Tcell4, verbose = FALSE)$RNA)
+clusters = data.frame(gene=rownames(avg.c1), c1=avg.c1, c3=avg.c2, c4=avg.c3, c5=avg.c4)
+colnames(clusters) = c("gene", "c1", "c2", "c3", "c4")
+
+#SuppTable26 = read.csv("/data/riazlab/projects/TCRseq/Input/Supplementary_Table_26.csv", sep=",")
+genes_focus = c("Macf1", "Cd55", "Fam78a", "Emp3", "S1pr1", "Jun", "Selenop", "Il6ra", "Ppp1r15a", 
+                "Tspan32", "St8sia6", "Ccr7", "Foxp1", "Actn1", "Tcf7", "Lef1", "Igfbp4", "Nacc2", 
+                "Tgfbr3", "Pdlim4", "Itm2a", "Ass1", "Cd40lg", "Cd4", "Zbtb7b", "Klf6", "Isg15", 
+                "Ifit3", "Gbp7", "Usp18", "Irf7", "Ifit1", "Irf1", "Gbp4", "Stat1", "Gbp2") # genes from Fig5d
+clusters = clusters[clusters$gene %in% genes_focus,]
+
+library(pheatmap)
+pdf("/data/riazlab/projects/TCRseq/output/Fig5d.v00.pdf")
+pheatmap(clusters[,c(2,3,4,5)], scale = "column",
+         cluster_rows = F, cluster_cols = T,
+         show_rownames = T
+)
+dev.off()
+
+### compare c1 and c4 ###
+c1_c4 = FindMarkers(Sdataobj2, ident.1 = "Cd4+ T cells 1", ident.2 = "Cd4+ T cells 4")
+head(c1_c4)
+## scatter correlation plot ##
+genes.to.label = c("Isg15","Ifit3","Gbp7","Usp18","Irf7","Irf1","Gbp4","Gbp2","Stat1")
+library(ggplot2)
+library(cowplot)
+pdf("/data/riazlab/projects/TCRseq/output/scatter.v00.pdf")
+p1 <- ggplot(clusters, aes(c1, c4)) + geom_point() + ggtitle("Comapre Cluster_1 and Cluster_4")
+p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
+p1 
+dev.off()
+#### Compare clusters Fig5 D #####
